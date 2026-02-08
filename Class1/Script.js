3eed5985877c0d5f55744f4ee7432b1752dd841a
JudeJetlearn <joefem5@gmail.com>
2026-02-08 15:49:30 +0000

    Add files via upload

---
+++ Class1/Script.js
@@ -0,0 +1,10 @@
+window.onresize = screenSizer
+window.onload = screenSizer
+
+function screenSizer() {
+
+    WIDTH = window.innerWidth
+    document.getElementById("size").innerHTML = WIDTH
+
+
+}
